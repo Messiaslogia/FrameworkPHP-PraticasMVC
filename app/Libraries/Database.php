120efb9dc0499451f18a0d2a8e5675647552fe59
messiaslogia <messias.logia@gmail.com>
2022-12-14 13:26:38 -0300

    Configurando o CRUD do programa

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -9,6 +9,8 @@ class Database{
     private $porta = '3306';
     private $dbh;
 
+    private $stmt;
+
     public function __construct()
     {
         //Fonte de dados ou DNS contém as informações necessarias para conectar ao banco de dados
@@ -29,4 +31,51 @@ class Database{
             die();
         }
     }
+
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);;
+    }
+
+    // Vincula um valor a um parâmetro
+    public function bind($parametro, $valor, $tipo = null)
+    {
+        if (is_null($tipo)):
+            switch (true):
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                    break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                    break;
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $tipo = PDO::PARAM_STR;
+            endswitch;
+        endif;
+
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }
+
+    public function executa()
+    {
+        return $this->stmt->execute();
+    }
+
+    public function resultados()        
+    {
+        $this->executa();
+    }
+
+    public function totalResultados()
+    {
+        return $this->stmt->rowCount();
+    }
+
+    public function ultimoIdInserido()
+    {
+        return $this->dbh->lastInsertId();
+    }
 }
+
